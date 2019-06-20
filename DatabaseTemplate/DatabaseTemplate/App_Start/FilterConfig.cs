840cfc09acd3e8c5629aea02c852a5f3e893f653
Unknown <19vign@elmbrookstudents.org>
2019-06-20 13:06:18 -0500

    init

---
+++ DatabaseTemplate/DatabaseTemplate/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DatabaseTemplate
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
